6c3ec0a0d65d712ff4b8ab05575d0bf7aedfe906
Mradul Varshney <80679669+mradulvarshney@users.noreply.github.com>
2022-08-22 08:31:58 +0530

    updated

--- Java_Super_150/src/Assignment_1/mirror_star_pattern.java
+++ Java_Super_150/src/Assignment_1/mirror_star_pattern.java
@@ -4,35 +4,39 @@ import java.util.Scanner;
 
 public class mirror_star_pattern
 {
-    public static void main(String args[]) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        for (int i = 0; i < n; i++) {
-            if(i<n/2 + 1){
-                for (int j = n - i -2; j > 0; j--) {
-                    System.out.print("  ");
-                }
-                for (int j = 0; j <= i; j++) {
-                    System.out.print("* ");
-                }
-                for(int j=0;j<i;j++){
-                    System.out.print("* ");
-                }
-            }
-            else{
-                System.out.println(i);
-                for (int j = i; j > 1; j--) {
-                    System.out.print("  ");
-                }
-
-                for (int j = 0; j < n-i; j++) {
-                    System.out.print("* ");
-                }
-                for(int j=0;j<n-i-1;j++){
-                    System.out.print("* ");
-                }
-            }
-            System.out.println();
-        }
+    public static void main(String args[])
+	{
+        Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int row=1;
+		int space=n/2;
+		int star=1;
+		while(row<=n)
+		{
+			int i=1;
+			while(i<=space)
+			{
+				System.out.print("  ");
+				i++;
+			}
+			int j = 1;
+			while(j<=star)
+			{
+				System.out.print("* ");
+				j++;
+			}
+			row++;
+			if(row<=(n/2)+1)
+			{
+				star+=2;
+				space--;
+			}
+			else
+			{
+				star-=2;
+				space++;
+			}
+			System.out.println();
+		}
     }
 }
