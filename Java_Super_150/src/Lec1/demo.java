63e414eb6d3d14e7b2f6a58c2c45cd2f07b136a8
mradulvarshney <mradulv26@gmail.com>
2022-09-14 23:23:30 +0530

    commit

--- Java_Super_150/src/Lec1/demo.java
+++ Java_Super_150/src/Lec1/demo.java
@@ -2,8 +2,7 @@ package Lec1;
 
 import java.util.Scanner;
 
-public class demo
-{
+public class demo {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
